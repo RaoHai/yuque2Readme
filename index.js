8d80f66de091e26759b81b6c90f8c3f6a40f9849
raohai.rh <raohai.rh@alibaba-inc.com>
2020-07-18 16:13:43 +0800

    feat: add generate logic

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { SiteClient } = require('datocms-client');
+(async function () {
+  try {
+    const datocmsToken = core.getInput('datocms-token');
+    const datocmsFilterModelIds = core.getInput('datocms-filter-model-ids') || '';
+    const datocmsPostUrl = core.getInput('datocms-post-url') || '';
 
-try {
-  const datocmsSiteUrl = core.getInput('datocms-site-url');
-  const datocmsToken =core.getInput('datocms-token');
+    const filterIds = datocmsFilterModelIds.split(',');
 
-  const client = new SiteClient(datocmsToken);
-  client.items.all({
-    'page[limit]': 5,
-    version: 'published'
-  }).then(items => {
-    console.log('->> items', items);
-  });
-} catch (e) {
-  core.setFailed(e.message);
-}
+    const client = new SiteClient(datocmsToken);
+
+    const itemTypes = (await client.itemTypes.all())
+      .filter(({ apiKey }) => filterIds.length ? filterIds.includes(apiKey) : true)
+      .map(({ id }) => id);
+
+    const filterdItems = (await client.items.all({ 'page[limit]': 5, version: 'published' }))
+      .filter(({ itemType }) => itemTypes.includes(itemType));
+
+    const content = filterdItems.map(post => (
+      `- [${post.title}](${datocmsPostUrl.replace(/\{\{(\w+)\}\}/g, (_, $) => (post[$]))})`
+    ));
+
+    console.log('>> content generated: ', content.join('\n'));
+
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+})();
