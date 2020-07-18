18f4c9d59982244b4e9804d14fd5b60e910c584c
raohai.rh <raohai.rh@alibaba-inc.com>
2020-07-18 17:10:40 +0800

    fix helper

--- dist/index.js
+++ dist/index.js
@@ -83717,8 +83717,8 @@ const { format } = __webpack_require__(2123);
 
 const { SiteClient } = __webpack_require__(1093);
 
-Handlebars.registerHelper('short', function (date) {
-  return format(new Date(date), 'MMMM DD');
+Handlebars.registerHelper('short', function (date, options) {
+  return format(new Date(date), options && options.format || 'MMMM dd');
 });
 
 (async function () {
